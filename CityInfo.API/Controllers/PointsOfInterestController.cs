a942d26d028d3140c251be93e26d34e562780714
Donald Feuzing <ntemmado@gmail.com>
2023-09-08 19:06:05 +0200

    init

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,70 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id.Equals(cityId));
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointofinterestid)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id.Equals(cityId));
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            //finf point of interest
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id.Equals(pointofinterestid));
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId, PointOfInterestForCreationDto pointOfInterestForCreationDto)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id.Equals(cityId));
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterestForCreationDto.Name,
+                Description = pointOfInterestForCreationDto.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+            return CreatedAtRoute("GetPointOfInterest", 
+                new
+                {
+                    cityId = cityId,
+                    pointOfInterestId = finalPointOfInterest.Id,
+                }, 
+                finalPointOfInterest);
+        }
+    }
+}
---
+++ CityInfo.API/Models/CitiesDataStore.cs
@@ -0,0 +1,83 @@
+﻿namespace CityInfo.API.Models
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; set; }  = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "montreal",
+                    Description = "quebec",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id =1,
+                            Name = "name 1",
+                            Description = "description 1" 
+                        },
+
+                        new PointOfInterestDto()
+                        {
+                            Id =2,
+                            Name = "name 2",
+                            Description = "description 2"
+                        }
+                    }
+                },
+
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "yaounde",
+                    Description = "cameroun",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id =3,
+                            Name = "name 3",
+                            Description = "description 3"
+                        },
+
+                        new PointOfInterestDto()
+                        {
+                            Id =4,
+                            Name = "name 4",
+                            Description = "description 4"
+                        }
+                    }
+                },
+
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "new york", 
+                    Description = "usa",
+                     PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id =5,
+                            Name = "name 5",
+                            Description = "description 5"
+                        },
+
+                        new PointOfInterestDto()
+                        {
+                            Id =6,
+                            Name = "name 6",
+                            Description = "description 6"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
